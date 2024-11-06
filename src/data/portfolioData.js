66b0b00b437e6f4e7d7cd1e65fda834ae9acadeb
youssou91 <youssougning@gmail.com>
2024-11-06 14:45:30 -0500

    Ajout projet perso

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,22 +1,22 @@
 export const portfolioData = [
   {
     id: 1,
-    name: 'Mutuelle de santé',
-    languages: ['javascript','Angular'],
-    languagesIcons: ['fab fa-js','fab fa-angular'],
+    name: 'E-commerce',
+    languages: ['javascript','php'],
+    languagesIcons: ['fab fa-js','fab fa-php'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    info: 'Ce site e-commerce est conçu avec PHP en programmation procédurale, où le code est structuré en fonctions sans la modularité des objets. La base de données, souvent construite en MySQL, stocke les informations des produits, utilisateurs, commandes et paiements. Les opérations s\’effectuent par requêtes SQL directes, et les données sont affichées dynamiquement à l\'aide de PHP intégré dans des fichiers HTML/CSS pour générer les pages.',
     // picture: './media/project1.JPG'
-    picture: '../media/emploiG.png'
+    picture: './media/imagephp.png'
   },
   {
     id: 2,
-    name: 'Gitech BTP',
+    name: 'Restaurant',
     languages: ['javascript','react'],
     languagesIcons: ['fab fa-js','fab fa-react'],
     source: 'http://www.github.com',
     info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.JPG'
+    picture: './media/imageRST.png',
   },
   {
     id: 3,
@@ -47,12 +47,12 @@ export const portfolioData = [
   },
   {
     id: 6,
-    name: 'Gitech Edu',
-    languages: ['javascript','php', 'Angular'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-angular'],
+    name: 'E-Learning',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js','fab fa-react'],
     source: 'http://www.github.com',
-    info: 'Gitech Edu est un progiciel de gestion intégrée qui, grâce à son système de production assistée et à de nombreux modules adaptés, automatise presque toutes les tâches courantes, et offre aux administrateurs des solutions performantes pour résoudre les différents problèmes rencontrés .La plateforme facilite le parcours scolaire de l’étudiant de son admission à sa sortie de l’établissement.',
-    picture: './media/project6.JPG'
+    info: 'Un site d\’e-learning développé avec React est une application web interactive et dynamique, où les utilisateurs peuvent s\'inscrire, suivre des cours, et interagir avec du contenu pédagogique en temps réel. Avec React, le site profite d\'une expérience utilisateur fluide, grâce aux composants réutilisables et à la mise à jour rapide de l\'interface sans rechargement de page',
+    picture: './media/imageEL.png'
   },
   {
     id: 7,
@@ -61,7 +61,7 @@ export const portfolioData = [
     languagesIcons: ['fab fa-js','fab fa-react'],
     source: 'http://www.github.com',
     info: 'Gitech Edu est un progiciel de gestion intégrée qui, grâce à son système de production assistée et à de nombreux modules adaptés, automatise presque toutes les tâches courantes, et offre aux administrateurs des solutions performantes pour résoudre les différents problèmes rencontrés .La plateforme facilite le parcours scolaire de l’étudiant de son admission à sa sortie de l’établissement.',
-    picture: './media/emploiG.png'
+    picture: './media/imageEMS.png'
   },
   {
     id: 8,
@@ -74,11 +74,11 @@ export const portfolioData = [
   },
   {
     id: 9,
-    name: 'Gitech Edu',
+    name: 'Touba',
     languages: ['javascript','react'],
     languagesIcons: ['fab fa-js','fab fa-react'],
     source: 'http://www.github.com',
     info: 'Gitech Edu est un progiciel de gestion intégrée qui, grâce à son système de production assistée et à de nombreux modules adaptés, automatise presque toutes les tâches courantes, et offre aux administrateurs des solutions performantes pour résoudre les différents problèmes rencontrés .La plateforme facilite le parcours scolaire de l’étudiant de son admission à sa sortie de l’établissement.',
-    picture: './media/project6.JPG'
+    picture: './media/imageTouba.png'
   },
 ]
