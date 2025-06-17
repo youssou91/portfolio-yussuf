fe12bf149cff26aad491e59e5bbadd5d6985c6a7
Youssou GNING <youssougning@gmail.com>
2025-06-16 23:29:07 -0400

    refonte et mis a jour

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -6,26 +6,25 @@ export const portfolioData = [
     languagesIcons: ['fab fa-js','fab fa-php'],
     source: 'http://www.github.com',
     info: 'Ce site e-commerce est conçu avec PHP en programmation procédurale, où le code est structuré en fonctions sans la modularité des objets. La base de données, souvent construite en MySQL, stocke les informations des produits, utilisateurs, commandes et paiements. Les opérations s\’effectuent par requêtes SQL directes, et les données sont affichées dynamiquement à l\'aide de PHP intégré dans des fichiers HTML/CSS pour générer les pages.',
-    // picture: './media/project1.JPG'
-    picture: './media/imagephp.png'
+    picture: './media/imagephp1.png'
   },
   {
     id: 2,
     name: 'Restaurant',
     languages: ['javascript','react'],
     languagesIcons: ['fab fa-js','fab fa-react'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/imageRST.png',
+    source: 'https://github.com/youssou91/Restau-REACT',
+    info: 'Ce site de restaurant est développé en React, une bibliothèque JavaScript pour construire des interfaces utilisateur dynamiques. Il utilise des composants réutilisables pour afficher les menus, les réservations et les avis clients. Les données peuvent être récupérées via des API REST ou GraphQL, permettant une interaction fluide et rapide. L\'interface est responsive, s\'adaptant aux différents appareils, et offre une expérience utilisateur moderne et intuitive.',
+    picture: './media/imageRestau.png',
   },
   {
     id: 3,
-    name: 'Pharos',
-    languages: ['javascript','php','Angular'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-angular'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    name: 'Bibliothèque Plus',
+    languages: ['javascript','php'],
+    languagesIcons: ['fab fa-js','fab fa-php'],
+    source: 'https://github.com/youssou91/Projet_Final',
+    info: 'Bibliothèque Plus est une application web de gestion de bibliothèque, développée en PHP avec une base de données MySQL. Elle permet aux utilisateurs de gérer les emprunts et retours de livres, de consulter le catalogue, et d\'effectuer des recherches avancées. L\'interface utilisateur est intuitive, facilitant la navigation et l\'accès aux fonctionnalités essentielles pour les bibliothécaires et les lecteurs.',
+    picture: './media/imageBiblio.png'
   },
   {
     id: 4,
@@ -38,12 +37,12 @@ export const portfolioData = [
   },
   {
     id: 5,
-    name: 'Hassmar',
-    languages: ['javascript','Angular'],
-    languagesIcons: ['fab fa-js', 'fab fa-angular'],
-    source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project5.JPG'
+    name: 'One Shop MVC',
+    languages: ['javascript','php'],
+    languagesIcons: ['fab fa-js','fab fa-php'],
+    source: 'https://github.com/youssou91/MVC_Composer',
+    info: 'One Shop MVC est un site e-commerce développé en PHP avec le modèle MVC (Modèle-Vue-Contrôleur). Il utilise Composer pour la gestion des dépendances et suit les principes de séparation des préoccupations. Le modèle gère les données et la logique métier, la vue affiche l\'interface utilisateur, et le contrôleur traite les requêtes et les interactions. La base de données est généralement gérée avec MySQL, permettant une gestion efficace des produits, utilisateurs et commandes.',
+    picture: './media/imagePHP2.png'
   },
   {
     id: 6,
